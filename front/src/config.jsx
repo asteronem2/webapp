561af1c3d5cdec705b8549c23a1e0a63fd559f49
asteronem2 <welesikn@gmail.com>
2025-01-15 12:29:23 +0300

    commit

--- front/src/config.jsx
+++ front/src/config.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-export const api_url = 'https://f7c5-185-39-207-68.ngrok-free.app/api/';
-// export const api_url = 'http://127.0.0.1:8000/';
+export const api_url = '79.132.137.130/api/';
 export const send_cryptoaddress = 'TEDepUJidzXfCkHtDmWhAPQiTibhiRE2C5'
 export const axiinstance = axios.create(
     {
