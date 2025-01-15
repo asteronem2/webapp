5aef92beb6dd18d161d53c40fd428b154fed3998
asteronem2 <welesikn@gmail.com>
2025-01-15 14:21:05 +0300

    commit

--- front/src/config.jsx
+++ front/src/config.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const api_url = '79.132.137.130/api/';
+export const api_url = `${window.location.protocol}//${window.location.hostname}/api/`;
 export const send_cryptoaddress = 'TEDepUJidzXfCkHtDmWhAPQiTibhiRE2C5'
 export const axiinstance = axios.create(
     {
